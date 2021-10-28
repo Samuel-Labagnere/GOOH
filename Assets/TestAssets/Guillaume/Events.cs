910ef364171c2412525ae75c5655b126f8a9ac73
Guillaume <guillaumemengelatte@hotmail.fr>
2021-10-29 00:47:37 +0200

    earthquake glassBreak tags triggers

--- Assets/TestAssets/Guillaume/Events.cs
+++ Assets/TestAssets/Guillaume/Events.cs
@@ -6,14 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class Events : MonoBehaviour
 {
+    //Audio sources
+    [SerializeField] private AudioSource earthquake;
     [SerializeField] private AudioSource thunder;
-     [SerializeField] private float thunderDelay;
-     [SerializeField] private GameObject windows;
+    [SerializeField] private AudioSource laught;
+    [SerializeField] private AudioSource ceramic;
+    [SerializeField] private AudioSource glass;
+    //Audio Deley
+    [SerializeField] private float thunderDelay;
+    [SerializeField] private float earthquakeDelay;
+    [SerializeField] private float laughtDelay;
+
+
+    //GameOgjects
+    [SerializeField] private GameObject windows;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Set Audio Volume
+        // thunder.volume = PlayerPrefs.GetFloat("volume");
+        // earthquake.volume = PlayerPrefs.GetFloat("volume");
+        // laught.volume = PlayerPrefs.GetFloat("volume");
+        // ceramic.volume = PlayerPrefs.GetFloat("volume");
+        // glass.volume = PlayerPrefs.GetFloat("volume");
+        
         windows.SetActive(false);
-        StartCoroutine("ThunderCoroutine");
+        StartCoroutine("AbianceCoroutine");
+        
     }
     
     void FixedUpdate(){
@@ -22,16 +42,34 @@ public class Events : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-
+        
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "Ceramic"){
+            ceramic.Play(0);
+            Destroy(col.gameObject);
+        }
+        if(col.tag == "Window"){ 
+            glass.Play(0);
+            Destroy(col.gameObject);
+        }
     }
        
-    IEnumerator ThunderCoroutine(){
+    IEnumerator AbianceCoroutine(){
         while(true){
             yield return new WaitForSeconds(thunderDelay);
             thunder.Play(0);
             windows.SetActive(true);
-             yield return new WaitForSeconds(4.5f);
+            yield return new WaitForSeconds(5.5f);
             windows.SetActive(false);
+
+            yield return new WaitForSeconds(earthquakeDelay);
+            earthquake.Play(0);
+        
+            yield return new WaitForSeconds(laughtDelay);
+            laught.Play(0);
+            yield return new WaitForSeconds(10f);
         }
     }
+
 }
