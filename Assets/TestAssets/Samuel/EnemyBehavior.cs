13c4a378280eef3f929e4e2f989f4b0c20c7e602
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-10-29 12:19:23 +0200

    GameOver with interactions

--- Assets/TestAssets/Samuel/EnemyBehavior.cs
+++ Assets/TestAssets/Samuel/EnemyBehavior.cs
@@ -10,6 +10,7 @@ public class EnemyBehavior : MonoBehaviour
     private float[] speedArray = new float[3];
     private Rigidbody2D rb2D;
     private GameObject enemy;
+    private GameObject deathField;
     private SpriteRenderer enemySprite;
     private Vector2 randomDirection;
     private bool invulnerable;
@@ -58,6 +59,7 @@ public class EnemyBehavior : MonoBehaviour
         EnemyStats.SetActive(false);
 
         enemy = gameObject;
+        deathField = enemy.transform.GetChild(0).gameObject;
         rb2D = enemy.GetComponent<Rigidbody2D>();
         speedArray[0] = -1f;
         speedArray[1] = 0f;
@@ -269,6 +271,7 @@ public class EnemyBehavior : MonoBehaviour
     }
 
     IEnumerator Run(){
+        deathField.SetActive(false);
         hitSound.Play();
         invulnerable = true;
         rb2D.AddForce(new Vector2(0f, 0f));
@@ -315,6 +318,7 @@ public class EnemyBehavior : MonoBehaviour
             invulnerable = false;
             speed = oldSpeed;
             direction = new Vector2(speedArray[Random.Range(0, 2)], speedArray[Random.Range(0, 2)]);
+            deathField.SetActive(true);
         }
     }
 
